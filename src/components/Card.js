de8e1b7379b50019224468629d53568727391cfe
Carlos Gutierrez <elimperiodeloefimero@gmail.com>
2020-06-17 22:25:31 -0500

    Incorporación de bootsrap, primer componente y estructura personalizada

---
+++ src/components/Card.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+import fittnesImage from '../images/exercise.png';
+import './styles/Card.css';
+
+class Card extends Component {
+  render () {
+    return(
+      <div className='card mx-auto Fitness-Card'>
+        <div className='card-body'>
+          <div className='row center'>
+            <div className='col-6'>
+              <img src={fittnesImage} alt='fit' />
+            </div>
+            <div className='col-6 Fitness-Card-Info'>
+              <h1>Technique Guides</h1>
+              <p>Learn amazing street workout and calisthenics</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Card;

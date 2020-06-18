e72ab3666d6a97a28417c05d87dbe034f6e0687a
Carlos Gutierrez <elimperiodeloefimero@gmail.com>
2020-06-18 15:00:02 -0500

    Se agrega data hardcode al estado de la pagina contenedora de los cards para iterar por los elementos posibles. Se corrige fix en style inline faltaba coma, si no se agrega simplemente no refleja el estilo en la UI

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
 
-import fittnesImage from '../images/exercise.png';
 import bgCicle from '../images/circles.png';
 import './styles/Card.css';
 
 class Card extends Component {
   render () {
-    const { bgRight, bgLeft, title, description } = this.props;
+    const { bgRight, bgLeft, bgImage, title, description } = this.props;
     return(
       <div className='card mx-auto Fitness-Card'
-        style={{ background: `url(${bgCicle}) linear-gradient(to right, ${bgRight} ${bgLeft})` }}>
+        style={{ backgroundImage: `url(${bgCicle}), linear-gradient(to right, ${bgRight}, ${bgLeft})` }}>
         <div className='card-body'>
           <div className='row center'>
             <div className='col-6'>
-              <img src={fittnesImage} alt='fit' />
+              <img src={bgImage} alt='fit' />
             </div>
             <div className='col-6 Fitness-Card-Info'>
               <h1>{title}</h1>

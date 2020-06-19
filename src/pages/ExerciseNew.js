43bacd31933e71d7a0dc1ebe575f6b74a7f08acb
Carlos Gutierrez <elimperiodeloefimero@gmail.com>
2020-06-18 21:04:44 -0500

    Se agrega formulario y eventos onSubmit al form y onChange al input a page ExerciseNew

--- src/pages/ExerciseNew.js
+++ src/pages/ExerciseNew.js
@@ -1,8 +1,85 @@
 import React, { Component } from 'react';
 
 class ExcerciseNew extends Component {
+  state = {}
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    })
+  }
+
   render() {
-    return(<div>New Excersice!</div>)
+    const { title, description, rightColor, img, leftColor } = this.state;
+    return (
+      <div className='container'>
+        <form onSubmit={this.handleSubmit}>
+          <div className='form-group'>
+            <input
+              type='text'
+              className='form-control'
+              placeholder='title'
+              name='title'
+              onChange={(e) => this.handleChange(e)}
+              value={title}
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='text'
+              className='form-control'
+              placeholder='description'
+              name='description'
+              onChange={(e) => this.handleChange(e)}
+              value={description}
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='text'
+              className='form-control'
+              placeholder='img'
+              name='img'
+              onChange={(e) => this.handleChange(e)}
+              value={img}
+            />
+          </div>
+          <div className='form-row'>
+            <div className='col'>
+              <input
+                type='text'
+                className='form-control'
+                placeholder='leftColor'
+                name='leftColor'
+                onChange={(e) => this.handleChange(e)}
+                value={leftColor}
+              />
+            </div>
+            <div className='col'>
+              <input
+                type='text'
+                className='form-control'
+                placeholder='rightColor'
+                name='rightColor'
+                onChange={(e) => this.handleChange(e)}
+                value={rightColor}
+              />
+            </div>
+          </div>
+
+          <button
+            type='submit'
+            className='btn btn-primary'
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    )
   }
 };
 

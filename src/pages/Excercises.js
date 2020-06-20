216cef62f43b9fb71a14be9564378229dbd8a98b
Carlos Gutierrez <elimperiodeloefimero@gmail.com>
2020-06-20 00:04:04 -0500

    Se agrega fases de request, loading, success, failed

--- src/pages/Excercises.js
+++ src/pages/Excercises.js
@@ -1,29 +1,51 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 import Welcome from '../components/Welcome';
 import ExcerciseList from '../components/ExcercisesList';
 import AddButton from '../components/AddButton';
+import Loading from '../components/Loading';
+import ErrorServer from './500';
+
 class Excercises extends Component {
   state = {
     data: [],
+    loading: true,
+    error: '',
   }
 
   async componentDidMount() {
-    const fetchData = await fetch('http://localhost:8000/api/exercises');
-    const responseJson = await fetchData.json();
+    try {
+      const fetchData = await fetch('http://localhost:8000/api/exercises');
+      const responseJson = await fetchData.json();
 
-    this.setState({data: responseJson});
+      this.setState({
+        data: responseJson,
+        loading: false,
+        error: '',
+      });
+      console.log('asdasd')
+    } catch (error) {
+      this.setState({
+        loading: false,
+        error: 'NO se pudo obtener la información, favor de intentar más tarde',
+      })
+    }
   }
 
-
   render() {
-    const { data } = this.state;
-    return (
-      <div>
-        <Welcome name='Carlos' />
-        <ExcerciseList data={data} />
-        <AddButton />
-      </ div>)
+    const { data, loading, error } = this.state;
+    if (loading) return <Loading />; 
+    if (error) {
+      return <ErrorServer />
+    } else {
+      return (
+        <div>
+          <Welcome name='Carlos' />
+          <ExcerciseList data={data} />
+          <AddButton />
+        </ div>
+      )
+    } 
   }
 }
 

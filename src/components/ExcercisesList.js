b492e90da790f4e7f3e6a1763570347b4f24b5e9
Carlos Gutierrez <elimperiodeloefimero@gmail.com>
2020-06-22 23:34:20 -0500

    destructurin en exerciseLIst

--- src/components/ExcercisesList.js
+++ src/components/ExcercisesList.js
@@ -5,13 +5,7 @@ import Card from './Card';
 const ExcerciseList = ({ data }) => (
   data.map(item => (
     <div key={item.id}>
-      <Card
-        rightColor={item.rightColor}
-        leftColor={item.leftColor}
-        image={item.img}
-        title={item.title}
-        description={item.description}
-      />
+      <Card {...item}/>
     </div>
   ))
 );

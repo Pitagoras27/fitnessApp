621081892d808488160bd55f1be74174a600be9f
Carlos Gutierrez <elimperiodeloefimero@gmail.com>
2020-06-22 23:17:21 -0500

    Se utiliza hook custom

---
+++ src/hooks/useFetchApi.js
@@ -0,0 +1,27 @@
+import {useState, useEffect} from 'react';
+
+const useFetchApi = url => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  
+  useEffect(() => {
+    const resultExercises = async () => {
+      try {
+        const fetchData = await fetch(url);
+        const responseJson = await fetchData.json();
+  
+        setData(responseJson);
+        setLoading(false);
+      } catch (error) {
+        setError('NO se pudo obtener la información, favor de intentar más tarde');
+        setLoading(false);
+      }
+    }
+    resultExercises();
+  },[url]);
+
+  return {data, loading, error}
+}
+
+export default useFetchApi;
--- src/pages/ExcerciseContainer.js
+++ src/pages/ExcerciseContainer.js
@@ -1,72 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 import Exercise from '../components/Exercise';
 import Loading from '../components/Loading';
+import useFetchApi from '../hooks/useFetchApi';
 import ErrorServer from './500';
 
 const ExerciseContainer = () => {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  
-  useEffect(() => {
-    const resultExercises = async () => {
-      try {
-        const fetchData = await fetch('http://localhost:8000/api/exercises');
-        const responseJson = await fetchData.json();
-  
-        setData(responseJson);
-        setLoading(false);
-      } catch (error) {
-        setError('NO se pudo obtener la información, favor de intentar más tarde');
-        setLoading(false);
-      }
-    }
-    resultExercises();
-  },[]);
+  const {loading, data, error} =
+  useFetchApi('http://localhost:8000/api/exercises');
 
   if (loading) return <Loading />; 
   if (error) return <ErrorServer />
   else return <Exercise data={data} />
 }
-// class ExcerciseContainer extends Component {
-//   state = {
-//     data: [],
-//     loading: true,
-//     error: '',
-//   }
-
-//   async componentDidMount() {
-//     try {
-//       const fetchData = await fetch('http://localhost:8000/api/exercises');
-//       const responseJson = await fetchData.json();
-
-//       this.setState({
-//         data: responseJson,
-//         loading: false,
-//         error: '',
-//       });
-
-//     } catch (error) {
-//       this.setState({
-//         loading: false,
-//         error: 'NO se pudo obtener la información, favor de intentar más tarde',
-//       })
-//     }
-//   }
-
-//   render() {
-//     const { data, loading, error } = this.state;
-
-//     if (loading) return <Loading />; 
-//     if (error) {
-//       return <ErrorServer />
-//     } else {
-//       return (
-//         <Exercise data={data} />
-//       )
-//     } 
-//   }
-// }
 
 export default ExerciseContainer;
